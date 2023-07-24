55fe8780584f7f488a2ead734c7e26aaaf41e0e7
timmoh-king <timothykariuki43@gmail.com>
2023-07-24 21:52:50 +0300

    update some part of our engine

--- models/city.py
+++ models/city.py
@@ -15,4 +15,4 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
-    places = relationship("Place", backref="user", cascade="delete")
+    places = relationship("Place", backref="cities", cascade="delete")
