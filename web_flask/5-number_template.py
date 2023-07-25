a694800ef132628307e1a31e8ab6e742d46097d6
timmoh-king <timothykariuki43@gmail.com>
2023-07-25 09:55:21 +0300

    5-number_template.py 6-number_odd_or_even.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/", strict_slashes=False)
@@ -40,7 +40,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """display a HTML page only if n is an integer"""
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
